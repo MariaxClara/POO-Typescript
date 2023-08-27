99a2767bd7a674e0fd1dc1af765de300b1075337
Maria Clara Almeida Galvao <78178074+MariaxClara@users.noreply.github.com>
2023-08-27 18:53:46 -0300

    classes

---
+++ Bicycle.ts
@@ -0,0 +1,9 @@
+class Bicycle {
+  model: string;
+
+  constructor(model: string) {
+    this.model = model;
+  }
+}
+
+export default Bicycle;
---
+++ Customer.ts
@@ -0,0 +1,9 @@
+class Customer {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+export default Customer;
